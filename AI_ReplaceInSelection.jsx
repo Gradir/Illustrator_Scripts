dce427edc8e94d0b2019abf6c90868aacb1b6b8e
Piotr Kołodziejczyk <gradir@gmail.com>
2020-11-11 22:38:14 +0100

    Replace in Selection

---
+++ AI_ReplaceInSelection.jsx
@@ -0,0 +1,27 @@
+﻿var doc = activeDocument;
+var selx = doc.selection;
+if(selx.length ==0){alert("You must select objects");}
+else{replace(selx);}
+
+function replace(sel)
+{
+	var active_doc = app.activeDocument;
+    var search_string = String(prompt("Replace with", "XXX"));
+    var replace_string = String(prompt("Replace with", "YYY"));
+
+	if (sel.length > 0)
+	{
+		for (var i = 0 ; i < sel.length; i++)
+		{
+			var this_text_frame = sel[i];
+			if ( this_text_frame.typename == "TextFrame" )
+			{
+				var new_string = this_text_frame.contents.replace(search_string, replace_string);
+				if (new_string != this_text_frame.contents)
+				{
+					this_text_frame.contents = new_string;
+				}
+			}
+		}
+	}
+}
